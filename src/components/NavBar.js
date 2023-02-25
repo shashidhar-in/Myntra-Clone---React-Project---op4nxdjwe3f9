286fca85eb10f4df7d584d11b77fd57d31b35b98
Shashidhar Mittapalli <shashidhar.mittapalli@gmail.com>
2023-02-26 04:20:19 +0530

    done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,19 +6,61 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import logo from "../myntra-logo.png"
 import { BiSearchAlt } from 'react-icons/bi';
-import { HiOutlineShoppingBag } from "react-icons/hi";import "./css/Nav.css"
+import { HiOutlineShoppingBag } from "react-icons/hi";
+import "./css/Nav.css"
+import { React, useContext} from 'react'
+import { Context } from './Context';
+import Cart from './Cart';
+import { useNavigate } from 'react-router-dom';
+
 
 
 function NavBar() {
+
+  const { setShow,cartCount,query,setQuery} = useContext(Context);
+  
+  const navigate = useNavigate();
+ 
+
+   console.log(query);
+
+
+  const search=(data)=>{
+    return data.filter((item)=> item.brand.toLowerCase().includes(query));
+  };
+  
+
+  function handleShow() {
+    setShow(true);
+  }
+  function navigateHome() {
+    navigate("/");
+  }
+
+
+
   return (
-    <Navbar className='shadow' bg="light" expand="lg" style={{ height: "80px" }}>
-      <Container fluid className='d-flex align-items-center'>
-        <Navbar.Brand ><div className='icon-holder'><Image src={logo} style={{ width: "65px" }} /></div></Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" style={{textAlign:"end"}}/>
-        <Navbar.Collapse id="navbarScroll">
+    <Navbar collapseOnSelect className='navbar shadow' bg="light" expand="lg" sticky='top' >
+      <Container fluid className='d-flex  align-items-center'>
+        <Navbar.Brand >
+          <div className='icon-holder' onClick={navigateHome} >
+            <Image id='logo' src={logo} />
+
+          </div>
+        </Navbar.Brand>
+        <div className='d-flex justify-content-end align-items-center'>
+          <div className='cart-sec cart-holder' id='cart-icon-small'  onClick={handleShow}>
+            <HiOutlineShoppingBag size={"2.5rem"}  className="cart-icon" />
+            <p className='cart-count cart-list-length'>{cartCount}</p>
+
+          </div>
+          <Navbar.Toggle aria-controls="navbarScroll" style={{ textAlign: "end" }} />
+        </div>
+
+        <Navbar.Collapse id="responsive-navbar-nav">
           <Nav
             className="me-auto my-2 my-lg-0 py-3"
-            navbarScroll
+
           >
             <Nav.Link ><h6>MEN</h6></Nav.Link>
             <Nav.Link ><h6>WOMEN</h6></Nav.Link>
@@ -26,15 +68,23 @@ function NavBar() {
 
 
           </Nav>
-          <div className='d-flex '><InputGroup className='search-box'>
-            <Form.Control aria-label="search-box" placeholder='Search' />
-            <Button className='search-button bg-light' style={{ border: "1px solid #ced4da" }}><BiSearchAlt size={"1.5rem"} id='search-icon' /></Button>
-          </InputGroup><HiOutlineShoppingBag size={"2.5rem"} className="cart-icon mx-3"/></div>
-          
+          <div className='d-flex '>
+            <InputGroup className='search-box'>
+            <Form.Control aria-label="search-box" placeholder='Search' onChange={(e)=>{setQuery((e.target.value).toLowerCase())}}/>
+            <Button className='search-button bg-light' style={{ border: "1px solid #ced4da" }}>
+              <BiSearchAlt size={"1.5rem"} id='search-icon' /></Button>
+          </InputGroup>
+           <div className='cart-sec cart-holder' id='cart-icon-large' onClick={handleShow}>
+            <HiOutlineShoppingBag size={"2.5rem"}  className="cart-icon" />
+            <p className='cart-count'>{cartCount}</p>
+
+          </div>
+            </div>
+
         </Navbar.Collapse>
-        
       </Container>
-      
+      <Cart />
+
     </Navbar>
   );
 }
